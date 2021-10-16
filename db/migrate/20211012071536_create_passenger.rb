b84d21d356292f64f61729b3546cdac4b2cc5b45
Martin Sugasti <56696387+MartinSugasti@users.noreply.github.com>
2021-10-16 09:52:49 +0200

    Add seats to seeds and minor changes in db. (#4)

--- db/migrate/20211012071536_create_passenger.rb
+++ db/migrate/20211012071536_create_passenger.rb
@@ -1,10 +1,10 @@
 class CreatePassenger < ActiveRecord::Migration[6.0]
   def change
     create_table :passengers do |t|
-      t.string :name
-      t.string :surname
-      t.string :passport
-      t.string :email
+      t.string :name, null: false
+      t.string :surname, null: false
+      t.string :passport, null: false, index: { unique: true }
+      t.string :email, null: false, index: { unique: true }
 
       t.timestamps
     end
